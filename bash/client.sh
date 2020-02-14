7cc349d9a9fa8d65688525b2f6b125a389b24f8b
Justin Trollip <jtrollip@gmail.com>
2020-02-14 16:18:18 +1100

    Restructure backend files

--- bash/client.sh
+++
@@ -1,9 +0,0 @@
-cd ../../
-cd client/common
-npm install
-cd ..
-echo Which web application do you want to run?
-read webapp
-cd $webapp
-npm install
-npm run start
--- gitea/bash.sh
+++
@@ -1,3 +0,0 @@
-sudo apt update
-sudo apt -y install git
-sudo snap install micro --classic
